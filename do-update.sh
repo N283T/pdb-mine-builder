d89996eeb5cabd447ac0c781490feb54b4ea14b2
Gert-Jan Bekker <gertjan.bekker@gmail.com>
2021-04-20 23:04:27 +0900

    updated fetch & config to new data system

--- do-update.sh
+++ do-update.sh
@@ -2,15 +2,13 @@
 # fetch data from pdbj's ftp
 mkdir -p data
 cd data
-rsync -a --delete ftp.pdbj.org::rsync/mine2/data/mmjson/noatom .
-rsync -a --delete ftp.pdbj.org::rsync/mine2/data/mmjson/plus-add/ plus
-rsync -a --delete ftp.pdbj.org::rsync/mine2/data/cc/mmjson/ cc
-rsync -a --delete ftp.pdbj.org::rsync/mine2/data/ccmodel/mmjson/ ccmodel
-rsync -a --delete ftp.pdbj.org::rsync/mine2/data/prd/mmjson/ prd
-rsync -a --delete ftp.pdbj.org::rsync/mine2/data/vrpt/mmcif/ vrpt
-wget https://raw.githubusercontent.com/yokochi47/pdbx-validation/master/schema/mmcif_pdbx_validation_v3.337.dic -O mmcif_pdbx_validation_v3.dic
+rsync -a --delete data.pdbjbk1.pdbj.org::rsync/pdbjplus/data/pdb/mmjson-noatom .
+rsync -a --delete data.pdbjbk1.pdbj.org::rsync/pdbjplus/data/pdb/mmjson-plus/ plus
+rsync -a --delete data.pdbjbk1.pdbj.org::rsync/pdbjplus/data/cc/mmjson/ cc
+rsync -a --delete data.pdbjbk1.pdbj.org::rsync/pdbjplus/data/ccmodel/mmjson/ ccmodel
+rsync -a --delete data.pdbjbk1.pdbj.org::rsync/pdbjplus/data/prd/mmjson/ prd
 cd ..
-rsync -a --delete ftp.pdbj.org::rsync/mine2/schemas .
+rsync -a --delete data.pdbjbk1.pdbj.org::rsync/pdbjplus/mine2/schemas .
 
 # RDB updater modules, comment out whatever is not required
 
@@ -25,6 +23,3 @@ node --max-old-space-size=8192 mine2.js ccmodel.load
 
 # update prd schema (BIRD, Biologically Interesting Molecule Reference Dictionary)
 node --max-old-space-size=8192 mine2.js prd.load
-
-# update vrpt schema (validation reports, pdbj version)
-node --max-old-space-size=8192 mine2.js vrpt.load
