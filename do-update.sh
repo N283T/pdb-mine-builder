daf52318094ea7632507c8c60ab22ddd27b5f30b
Gert-Jan Bekker <gertjan.bekker@protein.osaka-u.ac.jp>
2025-07-16 21:50:49 +0000

    changed lc1 to main for rsync fetch; enabled vrpt load

--- do-update.sh
+++ do-update.sh
@@ -2,16 +2,15 @@
 # fetch data from pdbj's ftp
 mkdir -p data
 cd data
-rsync -a --delete data.pdbjlc1.pdbj.org::rsync/pdbjplus/data/pdb/mmjson-noatom .
-rsync -a --delete data.pdbjlc1.pdbj.org::rsync/pdbjplus/data/pdb/mmjson-plus/ plus
-rsync -a --delete data.pdbjlc1.pdbj.org::rsync/pdbjplus/data/cc/mmjson/ cc
-rsync -a --delete data.pdbjlc1.pdbj.org::rsync/pdbjplus/data/ccmodel/mmjson/ ccmodel
-rsync -a --delete data.pdbjlc1.pdbj.org::rsync/pdbjplus/data/prd/mmjson/ prd
-rsync -a --delete data.pdbjlc1.pdbj.org::rsync/pdbjplus/data/prd/mmjson/ prd
-#rsync -a --delete data.pdbjlc1.pdbj.org::ftp/validation_reports/*/*/*_validation.cif.gz vrpt
+rsync -a --delete data.pdbj.org::rsync/pdbjplus/data/pdb/mmjson-noatom .
+rsync -a --delete data.pdbj.org::rsync/pdbjplus/data/pdb/mmjson-plus/ plus
+rsync -a --delete data.pdbj.org::rsync/pdbjplus/data/cc/mmjson/ cc
+rsync -a --delete data.pdbj.org::rsync/pdbjplus/data/ccmodel/mmjson/ ccmodel
+rsync -a --delete data.pdbj.org::rsync/pdbjplus/data/prd/mmjson/ prd
+rsync -a --delete data.pdbj.org::ftp/validation_reports/*/*/*_validation.cif.gz vrpt
 cd ..
-rsync -a --delete data.pdbjlc1.pdbj.org::rsync/pdbjplus/mine2/schemas .
-rsync -a --delete data.pdbjlc1.pdbj.org::rsync/pdbjplus/dictionaries .
+rsync -a --delete data.pdbj.org::rsync/pdbjplus/mine2/schemas .
+rsync -a --delete data.pdbj.org::rsync/pdbjplus/dictionaries .
 
 # RDB updater modules, comment out whatever is not required
 
@@ -28,4 +27,4 @@ node --max-old-space-size=8192 mine2.js ccmodel.load
 node --max-old-space-size=8192 mine2.js prd.load
 
 # update vrpt schema (wwPDB validation reports)
-#node --max-old-space-size=8192 mine2.js vrpt.load
+node --max-old-space-size=8192 mine2.js vrpt.load
